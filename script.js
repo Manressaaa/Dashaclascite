fa91e01c53e45f49d46178cc9a1e73d4990fab24
Manressaaa <dashaglebko34@gmail.com>
2025-12-25 19:45:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,38 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+$(document).ready(function() {
+  let currentIndex = 0;
+  const items = $(".testimonial-item");
+  const dots = $(".dot");
+
+  function showSlide(index) {
+    items.removeClass("active").eq(index).addClass("active");
+    dots.removeClass("active").eq(index).addClass("active");
+  }
+
+  dots.on("click", function() {
+    currentIndex = $(this).index();
+    showSlide(currentIndex);
+  });
+
+  $(".arrow.prev").on("click", function() {
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    showSlide(currentIndex);
+  });
+
+  $(".arrow.next").on("click", function() {
+    currentIndex = (currentIndex + 1) % items.length;
+    showSlide(currentIndex);
+  });
+
+  setInterval(function() {
+    currentIndex = (currentIndex + 1) % items.length;
+    showSlide(currentIndex);
+  }, 5000);
+
+  showSlide(currentIndex);
+});
+
+
+
